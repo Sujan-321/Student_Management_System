d2afe689913a95a5985fc031f8538ac534925bdf
Sujan <sujanghimire002@gmail.com>
2026-07-01 21:14:02 +0545

    create register view and urls for login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import UserRegistrationForm
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = "accounts/register.html"
+    success_url = reverse_lazy("login")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        messages.success(
+            self.request,
+            "Your account has been created successfully. Please log in."
+        )
+
+        return response
