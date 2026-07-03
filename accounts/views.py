4b3f228e6193608b538f80c4341716e6ff3e86e3
Sujan <sujanghimire002@gmail.com>
2026-07-03 22:14:16 +0545

    creating a logout view and update the urls.py file of accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView 
 
 from .forms import UserRegistrationForm, UserLoginForm
 
@@ -42,3 +42,9 @@ class UserLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy("home")
+
+class UserLogoutView(LogoutView):
+
+    def dispatch(self, request, *args, **kwargs):
+        messages.success(request, "Logged out successfully.")
+        return super().dispatch(request, *args, **kwargs)
