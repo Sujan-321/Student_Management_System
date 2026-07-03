fa21dc417af1b1de4706cb5200352cdc80f6b03b
Sujan <sujanghimire002@gmail.com>
2026-07-03 22:09:45 +0545

    create LoginView in accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,9 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
 
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 
 
 class RegisterView(CreateView):
@@ -21,3 +22,23 @@ class RegisterView(CreateView):
         )
 
         return response
+
+
+
+
+class UserLoginView(LoginView):
+    template_name = "accounts/login.html"
+    authentication_form = UserLoginForm
+
+    def form_valid(self, form):
+        remember_me = form.cleaned_data.get("remember_me")
+
+        if not remember_me:
+            self.request.session.set_expiry(0)
+
+        messages.success(self.request, "Logged in successfully.")
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy("home")
