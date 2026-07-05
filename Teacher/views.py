3343ba959b757b8c0a6d1d6d271bc2b5ef77af08
Sujan <sujanghimire002@gmail.com>
2026-07-05 22:36:19 +0545

    creating teacher/urls.py and views.py file

--- Teacher/views.py
+++ Teacher/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, UpdateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Teacher
+from .forms import TeacherProfileForm
+
+
+class TeacherDashboardView(LoginRequiredMixin, TemplateView):
+    template_name = "Teacher/dashboard.html"
+
+
+class TeacherProfileView(LoginRequiredMixin, UpdateView):
+    model = Teacher
+    form_class = TeacherProfileForm
+    template_name = "Teacher/profile.html"
+    success_url = reverse_lazy("teacher_profile")
+
+    def get_object(self):
+        return Teacher.objects.get(user=self.request.user)
