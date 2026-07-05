4a32887a833acdd9524316dc4d164829ec503af5
Sujan <sujanghimire002@gmail.com>
2026-07-05 22:03:55 +0545

    creating a home.html file

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 from django.contrib.auth.views import LoginView, LogoutView 
 
 from django.contrib import messages
@@ -15,6 +15,10 @@ from django.contrib.auth.views import (
 )
 
 from .forms import UserRegistrationForm, UserLoginForm
+
+
+class HomeView(TemplateView):
+    template_name = "accounts/home.html"
 
 
 class RegisterView(CreateView):
