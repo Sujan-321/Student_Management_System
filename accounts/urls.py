a7e781d21658be9aa743e4fa74bf2e477e78af1c
Sujan <sujanghimire002@gmail.com>
2026-07-05 22:23:37 +0545

    update the accounts/urls.py file

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import RegisterView, UserLogoutView, UserLoginView
+from .views import RegisterView, UserLogoutView, UserLoginView, HomeView, UserPasswordChangeView
 
 urlpatterns = [
     path("", UserLoginView.as_view(), name="login"),
+    path("/home", HomeView.as_view(), name="home"),
     path("login/", UserLoginView.as_view(), name="login"),
     path("register/", RegisterView.as_view(), name="register"),
     path("logout/", UserLogoutView.as_view(), name="logout"),
+    path("password-change/", UserPasswordChangeView.as_view(), name="password_change"),
 ]
