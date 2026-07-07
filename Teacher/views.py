76d8aa99341f8bfc5169c8c44080949dd7dec345
Sujan <sujanghimire002@gmail.com>
2026-07-07 10:00:12 +0545

    update the Teacher/views.py file

--- Teacher/views.py
+++ Teacher/views.py
@@ -2,19 +2,34 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView, UpdateView
 from django.urls import reverse_lazy
 
-from .models import Teacher
 from .forms import TeacherProfileForm
+from .models import Teacher
 
 
 class TeacherDashboardView(LoginRequiredMixin, TemplateView):
+
     template_name = "Teacher/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        context["teacher"] = Teacher.objects.get(user=self.request.user)
+
+        context["student_count"] = 0
+        context["class_count"] = 0
+        context["subject_count"] = 0
+
+        return context
 
 
 class TeacherProfileView(LoginRequiredMixin, UpdateView):
+
     model = Teacher
     form_class = TeacherProfileForm
     template_name = "Teacher/profile.html"
     success_url = reverse_lazy("teacher_profile")
 
     def get_object(self):
+
         return Teacher.objects.get(user=self.request.user)
