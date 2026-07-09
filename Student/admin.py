54905d53414bf6792fe32e529a900f01af9c3f3a
Sujan <sujanghimire002@gmail.com>
2026-07-09 13:15:12 +0545

    create student/admin.py file more specific with proper displaying data.

--- Student/admin.py
+++ Student/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Student, Department
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "code",
+    )
+
+    search_fields = (
+        "name",
+        "code",
+    )
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "roll_number",
+        "first_name",
+        "last_name",
+        "department",
+        "phone_number",
+        "status",
+    )
+
+    list_filter = (
+        "department",
+        "gender",
+        "status",
+    )
+
+    search_fields = (
+        "first_name",
+        "last_name",
+        "roll_number",
+        "registration_number",
+    )
