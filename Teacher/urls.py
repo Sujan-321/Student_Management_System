8b99613f7dbcf1381874448ae201ea4dc95fdaad
Sujan <sujanghimire002@gmail.com>
2026-07-15 23:55:35 +0545

    creating assignment_list.html file in Teacher app.

--- Teacher/urls.py
+++ Teacher/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from . import views
+from .views import TeacherDashboardView, TeacherProfileView, AssignmentListView, AssignmentCreateView, AssignmentUpdateView, AssignmentDeleteView
 
 urlpatterns = [
-    path("dashboard/", views.TeacherDashboardView.as_view(), name="teacher_dashboard"),
-    path("profile/", views.TeacherProfileView.as_view(), name="teacher_profile"),
+    path("dashboard/", TeacherDashboardView.as_view(), name="teacher_dashboard"),
+    path("profile/", TeacherProfileView.as_view(), name="teacher_profile"),
+    path("assignments/", AssignmentListView.as_view(), name="assignment_list"),
+    path("assignments/create/", AssignmentCreateView.as_view(), name="assignment_create"),
+    path("assignments/<int:pk>/update/", AssignmentUpdateView.as_view(), name="assignment_update"),
+    path("assignments/<int:pk>/delete/", AssignmentDeleteView.as_view(), name="assignment_delete"),
 ]
